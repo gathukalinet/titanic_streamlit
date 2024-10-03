8a5a46dc4af08c9229f615ca49cbc1a686d5d01d
gathukalinet <160579977+gathukalinet@users.noreply.github.com>
2024-10-03 14:51:33 +0300

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -18,4 +18,4 @@ st.title('Bar on Count of Sex by Passenger Class')
 st.bar_chart(dff, x='Pclass', y='count', color='Sex', stack=False, x_label='Passenger Class', y_label='Number of Passengers')
 
 st.title('Bar on Count of Sex by Passenger Survival Status')
-st.bar_chart(dff, x='Survived', y='count', color='Sex', stack=False, horizontal=True, x_label='Number of Passengers', y_label='Survived')s
+st.bar_chart(dff, x='Survived', y='count', color='Sex', stack=False, horizontal=True, x_label='Number of Passengers', y_label='Survived')
